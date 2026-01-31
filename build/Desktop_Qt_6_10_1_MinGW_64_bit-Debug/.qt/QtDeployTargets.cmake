77023860e6bccf13b3fa051686853523e9f3d1dc
WeN1nG <3266038380@qq.com>
2026-01-31 19:07:36 +0800

    删除构造文件

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_SongPlayer_FILE C:/CODE/QTS/Projects/SongPlayer/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/SongPlayer.exe)
-set(__QT_DEPLOY_TARGET_SongPlayer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_SongPlayer_RUNTIME_DLLS C:/D/software/QT/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/D/software/QT/6.10.1/mingw_64/bin/Qt6Multimedia.dll;C:/D/software/QT/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/D/software/QT/6.10.1/mingw_64/bin/Qt6Network.dll;C:/D/software/QT/6.10.1/mingw_64/bin/Qt6Concurrent.dll;C:/D/software/QT/6.10.1/mingw_64/bin/Qt6Core.dll)
